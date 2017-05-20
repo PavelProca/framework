dc0224cd446d156577f6f478a2fa97c40acfc7e2
Proca Pavel <proka.pavel@gmail.com>
2017-05-20 21:45:33 +0300

    added mega input

--- script/script.js
+++ script/script.js
@@ -1,3 +1,12 @@
 function tmp(parametr) {
 	document.getElementsByClassName('right_panel')[0].style.background = '#1E242A';
 }
+
+var app = {
+	name: 'Hello world',
+  prev: 'Hello world',
+  save: () => {app.prev = app.name; app.v=0},
+  cancel: () => {app.name = app.prev; app.v=0}
+};
+
+alight('body', app);
